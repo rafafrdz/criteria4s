ce3b78ae404038d7ca50781f26efa3ae334c1e2d
Rafael Fernández <rafaelfernandezortiz@gmail.com>
2024-02-25 09:31:45 +0100

    using Sym to create lit and col

--- core/src/main/scala/io/github/rafafrdz/criteria4s/instances/package.scala
+++ core/src/main/scala/io/github/rafafrdz/criteria4s/instances/package.scala
@@ -1,9 +1,16 @@
 package io.github.rafafrdz.criteria4s
 
 import io.github.rafafrdz.criteria4s.core.Criteria.CriteriaTag
+import io.github.rafafrdz.criteria4s.core.Sym
 import io.github.rafafrdz.criteria4s.instances.builder.{Builder1, Builder2}
 
 package object instances {
+  def sym0[T <: CriteriaTag]: Sym[T] = sym1(v => v)
+
+  def sym1[T <: CriteriaTag](symF: String => String): Sym[T] = sym(symF, symF)
+  def sym[T <: CriteriaTag](colF: String => String, valueF: String => String): Sym[T] =
+    Sym.sym(colF, valueF)
+
   def build[T <: CriteriaTag, H[_ <: CriteriaTag]](F: (String, String) => String)(implicit
       BH: Builder2[H]
   ): H[T] =
