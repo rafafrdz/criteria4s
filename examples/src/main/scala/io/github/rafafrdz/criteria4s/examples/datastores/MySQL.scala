2285b287d3ff7f0065576a8ec45bdc17e8e12e1b
Rafael Fernández <rafaelfernandezortiz@gmail.com>
2024-02-25 09:31:59 +0100

    integrate Sym

--- examples/src/main/scala/io/github/rafafrdz/criteria4s/examples/datastores/MySQL.scala
+++ examples/src/main/scala/io/github/rafafrdz/criteria4s/examples/datastores/MySQL.scala
@@ -3,7 +3,6 @@ package io.github.rafafrdz.criteria4s.examples.datastores
 import io.github.rafafrdz.criteria4s.core.PredOp._
 import io.github.rafafrdz.criteria4s.instances._
 import io.github.rafafrdz.criteria4s.sql._
-import io.github.rafafrdz.criteria4s.sql.SQL
 
 trait MySQL extends SQL
 
@@ -11,7 +10,10 @@ object MySQL extends SQLExpr[MySQL] {
 
   private def opeExpr(symbol: String)(a: String, b: String): String = s"($a $symbol $b)"
 
-  // That's not the right symbol for MySQL but it's just an example of how to override the default implementation
+  /**
+   * That's not the right symbol for MySQL but it's just an example of how to override the default
+   * implementation
+   */
   implicit override val leqPred: LEQ[MySQL] = build[MySQL, LEQ](opeExpr("<<<"))
 
 }
--- examples/src/main/scala/io/github/rafafrdz/criteria4s/examples/datastores/Postgres.scala
+++ examples/src/main/scala/io/github/rafafrdz/criteria4s/examples/datastores/Postgres.scala
@@ -1,7 +1,18 @@
 package io.github.rafafrdz.criteria4s.examples.datastores
 
-import io.github.rafafrdz.criteria4s.sql._
-import io.github.rafafrdz.criteria4s.sql.SQL
+import io.github.rafafrdz.criteria4s.core._
+import io.github.rafafrdz.criteria4s.instances._
+import io.github.rafafrdz.criteria4s.sql.{SQL, _}
 
-trait Postgres extends SQL
-object Postgres extends SQLExpr[Postgres]
+trait Postgres  extends SQL
+object Postgres extends SQLExpr[Postgres] {
+
+  /**
+   * That's not the right symbol for Postgres but it's just an example of how to override the
+   * default implementation
+   */
+
+  val C: String => String                     = s => s""""!$s!""""
+  val V: String => String                     = s => s"""`$s`"""
+  override implicit val symRef: Sym[Postgres] = sym[Postgres](C, V)
+}
